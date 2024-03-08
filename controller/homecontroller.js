3ae7a07ad7c46a6a08618958de287e680065f061
Akash Krushna Barik <akashkrushna333@gmail.com>
2024-03-08 22:12:17 +0530

    Add new features and update files

--- controller/homecontroller.js
+++ controller/homecontroller.js
@@ -1,113 +1,87 @@
-const TodoLists = require('../models/todo_list')
+const TodoLists = require('../models/todo_list');
 
-// function for redirecting to main home page
-module.exports.home = function(req, res) {
-    // Fetching using mongoose
-    TodoLists.find({})
-        .then(todo => {
-            return res.render('homePage', {
-                title: "Home",
-                todoList: todo
-            });
-        })
-        .catch(err => {
-            console.log('Error in fetching data:', err);
-            return res.status(500).send('Internal Server Error');
+// Function for redirecting to main home page
+module.exports.home = async function(req, res) {
+    try {
+        const todo = await TodoLists.find({});
+        return res.render('homePage', {
+            title: "Home",
+            todoList: todo
         });
-}
-// function for new Data
-function DateValeu(dueDate){
-    let months = ['jan','feb','mar','Apr','May','june','july','aug','sept','oct','nov','dec'] // static value for implementing monthe value
-
-
-    newdate = '';
-    let monapp = '';
-    // checking months 
-    if(dueDate[1] == '01'){
-        monapp=months[0];
+    } catch (err) {
+        console.log('Error in fetching data:', err);
+        return res.status(500).send('Internal Server Error');
     }
-    else if(dueDate[1] == '02'){
-        monapp=months[1];
-    }else if(dueDate[1] == '03'){
-        monapp=months[2];
-    }else if(dueDate[1] == '04'){
-        monapp=months[3];
-    }else if(dueDate[1] == '04'){
-        monapp=months[3];
-    }else if(dueDate[1] == '05'){
-        monapp=months[4];
-    }else if(dueDate[1] == '06'){
-        monapp=months[5];
-    }else if(dueDate[1] == '07'){
-        monapp=months[6];
-    }else if(dueDate[1] == '08'){
-        monapp=months[7];
-    }else if(dueDate[1] == '09'){
-        monapp=months[8];
-    }else if(dueDate[1] == '10'){
-        monapp=months[9];
-    }else if(dueDate[1] == '11'){
-        monapp=months[10];
-    }else if(dueDate[1] == '12'){
-        monapp=months[11];
+}
+
+// Function for creating todo list
+module.exports.createTodo = async function(req, res) {
+    try {
+        const dueDate = req.body.dateValue.split('-');
+        const newdate = DateValue(dueDate);
+
+        await TodoLists.create({
+            desc: req.body.desc,
+            category: req.body.category,
+            dueDate: newdate
+        });
+        
+        return res.redirect('/');
+    } catch (err) {
+        console.log('Oops, error occurred:', err);
+        return res.status(500).send('Internal Server Error');
     }
-    newdate =dueDate[2]+'-'+monapp+'-'+dueDate[0] // displaying date in dd-mm-yyyy formata
-    return newdate;
 }
 
-// function for creating toto list
-module.exports.createTodo = function(req,res){
-    dueDate =req.body.dateValue.split('-'); // splitting date and taking months value
-   let newdate='';
-    newdate= DateValeu(dueDate);     
-    TodoLists.create({ // crating new todo and storing into DB
-        desc:req.body.desc,
-        category:req.body.category,
-        dueDate: newdate
-    },function(err,newArr){
-        if(err){
-            console.log('Oops error occoured');
-            return;
-        }
-        return res.redirect('/')
-    })
-}
-
-// function for deleting todo list
-module.exports.deleteTodo = function(req,res){ 
-    sp = req.query.id; // getting the id from ui
-    newsp = sp.split(','); 
-    for(let i=0;i<newsp.length;i++){ // looping over newsp  to delete all the checked value
-        TodoLists.findByIdAndDelete(newsp[i],function(err){
-            if(err){
-                console.log('err')
-                return;
-            }
-        })
+// Function for deleting todo list
+module.exports.deleteTodo = async function(req, res) {
+    try {
+        const ids = req.query.id.split(',');
+        await TodoLists.deleteMany({ _id: { $in: ids } });
+        return res.redirect('/');
+    } catch (err) {
+        console.log('Error while deleting:', err);
+        return res.status(500).send('Internal Server Error');
     }
-return res.redirect('/');
 }
 
-// function for fetching data for edit page
-module.exports.EditPage = function(req,res){ // here we are fetching the data whic need to be edited
-    console.log('aaa',req.query)
-    TodoLists.findById(req.query.id,function(err,todoLists){
-        if(err){ console.log('hi man!! it an error'); return}
-        return res.render('editPage',{
-        title:'Edit Page',
-        todolist:todoLists
-        })
-    })
+// Function for fetching data for edit page
+module.exports.EditPage = async function(req, res) {
+    try {
+        const todoLists = await TodoLists.findById(req.query.id);
+        return res.render('editPage', {
+            title: 'Edit Page',
+            todolist: todoLists
+        });
+    } catch (err) {
+        console.log('Error while fetching data:', err);
+        return res.status(500).send('Internal Server Error');
+    }
 }
 
-// function for updatind tada after the todo is being edited
-module.exports.editDetails = function(req,res){
-    dueDate =req.body.dueDate.split('-'); // splitting date and taking montha value
-    let newdate='';
-    newdate= DateValeu(dueDate);     
-     TodoLists.updateOne({_id:req.query.id},{$set:{desc:req.body.desc,category:req.body.category,dueDate:newdate}} ,function(err,todoData){
-        if(err){console.log('erroe while updating'); return}
-        return res.redirect('/')
-    })
+// Function for updating todo data after editing
+module.exports.editDetails = async function(req, res) {
+    try {
+        const dueDate = req.body.dueDate.split('-');
+        const newdate = DateValue(dueDate);
+        
+        await TodoLists.updateOne({ _id: req.query.id }, {
+            $set: { desc: req.body.desc, category: req.body.category, dueDate: newdate }
+        });
+        
+        return res.redirect('/');
+    } catch (err) {
+        console.log('Error while updating:', err);
+        return res.status(500).send('Internal Server Error');
+    }
 }
+
+// Function to format date
+function DateValue(dueDate) {
+    const months = ['jan','feb','mar','Apr','May','june','july','aug','sept','oct','nov','dec'];
+    const monthIndex = parseInt(dueDate[1]) - 1;
+    const month = months[monthIndex];
+    return dueDate[2] + '-' + month + '-' + dueDate[0];
+}
+
 
